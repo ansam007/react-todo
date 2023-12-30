c2d6daa370bec2781b00f8cc503dbe0e4abda68d
ansam007 <anshul.sharma5271@gmail.com>
2023-12-30 12:41:36 +0530

    used CSS modules for styling

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,25 +1,39 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: ${props=>(props.isValid ? "#FF7276" : "#ac0e77")};
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// const Button = styled.button`
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: ${props=>(props.isValid ? "#FF7276" : "#ac0e77")};
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+//   width: 100%;
 
+//   @media(min-width : 768px) {
+//     width : auto;
+//   }
 
-&:focus {
-  outline: none;
+// &:focus {
+//   outline: none;
+// }
+
+// &:hover,
+// &:active {
+//   background: #ac0e77;
+//   border-color: #ac0e77;
+//   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+// }
+// `;
+
+import styles from './Button.module.css';
+const Button = (props) => {
+  let valid = props.isValid;
+  return (
+    <button type={props.type} style={{background : valid ? '#FF7276' : '#ac0e77'}} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  )
 }
-
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
-`;
 
 export default Button;
